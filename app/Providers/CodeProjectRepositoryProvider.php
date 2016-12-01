2d758120377e1e66a484a894623866aa63c1e1d6
William Brito <williamrldb@gmail.com>
2016-12-01 18:51:25 -0200

    RELACIONANDO MODELS - Criando ProjectNote

--- app/Providers/CodeProjectRepositoryProvider.php
+++ app/Providers/CodeProjectRepositoryProvider.php
@@ -33,5 +33,10 @@ class CodeProjectRepositoryProvider extends ServiceProvider
             \CodeProject\Repositories\ProjectRepositoryEloquent::class
         );
 
+        $this->app->bind(
+            \CodeProject\Repositories\ProjectNoteRepository::class,
+            \CodeProject\Repositories\ProjectNoteRepositoryEloquent::class
+        );
+
     }
 }
