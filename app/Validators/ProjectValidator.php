b80e3bb8513090bee0f47f0d1c31cdeb0c5656dc
William Brito <williamrldb@gmail.com>
2016-12-01 18:15:44 -0200

    RELACIONANDO MODELS - Criando ProjectService

---
+++ app/Validators/ProjectValidator.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brito
+ * Date: 01/12/2016
+ * Time: 16:15
+ */
+
+namespace CodeProject\Validators;
+
+
+use Prettus\Validator\LaravelValidator;
+
+class ProjectValidator extends LaravelValidator
+{
+    protected $rules = [
+        'owner_id' => 'required',
+        'client_id' => 'required',
+        'name' => 'required',
+        'progress' => 'required',
+        'status' => 'required',
+        'due_date' => 'required'
+    ];
+}
