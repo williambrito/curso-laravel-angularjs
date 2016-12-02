a6160a54a5ed37204f8a1c64068ce125120ce4a8
William Brito <williamrldb@gmail.com>
2016-12-02 13:40:38 -0300

    OAUTH2 - Criando Rota para o access token

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,6 +11,12 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+        factory(CodeProject\Entities\User::class)->create([
+            'name' => 'William',
+            'email' => 'williamrldb@gmail.com',
+            'password' => bcrypt(123456),
+            'remember_token' => str_random(10),
+        ]);
         factory(CodeProject\Entities\User::class, 10)->create();
     }
 }
