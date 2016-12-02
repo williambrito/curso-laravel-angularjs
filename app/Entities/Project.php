38ebb692fd5acedfe6405a3f782f5c0c13a07c74
William Brito <williamrldb@gmail.com>
2016-12-02 18:10:05 -0300

    AUTORIZACAO - Relacionando ProjectMembers

--- app/Entities/Project.php
+++ app/Entities/Project.php
@@ -26,4 +26,9 @@ class Project extends Model implements Transformable
         return $this->hasMany(ProjectNote::class);
     }
 
+    public function members()
+    {
+        return $this->belongsToMany(User::class,'project_members', 'project_id', 'member_id');
+    }
+
 }
---
+++ app/Entities/ProjectMember.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CodeProject\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectMember extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'member_id',
+    ];
+
+}
