ad1c461ca2bd5d9a43a0fb056b33bff8bf26af36
William Brito <williamrldb@gmail.com>
2016-12-02 14:35:44 -0300

    OAUTH2 - Agrupando rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,21 +19,17 @@ Route::post('oauth/access_token', function() {
     return Response::json(Authorizer::issueAccessToken());
 });
 
-Route::get('client', ['middleware'=>'oauth', 'uses'=>'ClientController@index']);
+Route::group(['middleware'=>'oauth'], function (){
+    Route::resource('client', 'ClientController', ['except'=>['create', 'edit']]);
 
-Route::post('client', 'ClientController@store');
-Route::get('client/{id}', 'ClientController@show');
-Route::put('client/{id}', 'ClientController@update');
-Route::delete('client/{id}', 'ClientController@destroy');
+    Route::group(['prefix'=>'project'], function (){
+        Route::resource('', 'ProjectController', ['except'=>['create', 'edit']]);
 
-Route::get('project/{id}/note', 'ProjectNoteController@index');
-Route::post('project/{id}/note', 'ProjectNoteController@store');
-Route::get('project/{id}/note/{noteId}', 'ProjectNoteController@show');
-Route::put('project/{id}/note/{noteId}', 'ProjectNoteController@update');
-Route::delete('project/{id}/note/{noteId}', 'ProjectNoteController@destroy');
+        Route::get('{id}/note', 'ProjectNoteController@index');
+        Route::post('{id}/note', 'ProjectNoteController@store');
+        Route::get('{id}/note/{noteId}', 'ProjectNoteController@show');
+        Route::put('{id}/note/{noteId}', 'ProjectNoteController@update');
+        Route::delete('{id}/note/{noteId}', 'ProjectNoteController@destroy');
+    });
+});
 
-Route::get('project', 'ProjectController@index');
-Route::post('project', 'ProjectController@store');
-Route::get('project/{id}', 'ProjectController@show');
-Route::put('project/{id}', 'ProjectController@update');
-Route::delete('project/{id}', 'ProjectController@destroy');
