34a7fac12d50d5641d4311d83af1610f64828a27
William Brito <williamrldb@gmail.com>
2016-12-03 18:42:48 -0300

    ANGULAR -> CONFIGURACAO DE AMBIENTE -> Declarando dependências na página de boot

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,10 @@
 	<title>Laravel</title>
 
 	@if(Config::get('app.debug'))
-
+		<link href="{{ asset('build/css/vendor/bootstrap.min.css') }}" rel="stylesheet">
+		<link href="{{ asset('build/css/vendor/bootstrap-theme.min.css') }}" rel="stylesheet">
 	@else
-
+		<link href="{{ elixir('css/all.css') }}">
 	@endif
 
 	<!-- Fonts -->
@@ -65,9 +66,16 @@
 
 	<!-- Scripts -->
 	@if(Config::get('app.debug'))
-
+		<script src="{{ asset('build/js/vendor/jquery.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular-route.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular-resource.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular-animate.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/angular-messages.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/ui-bootstrap.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/navbar.min.js') }}"></script>
 	@else
-
+		<script src="{{ elixir('js/all.js') }}"></script>
 	@endif
 </body>
 </html>
