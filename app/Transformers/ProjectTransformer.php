2503c96eeb6e147a05b9e43bae100a416ae836c2
William Brito <williamrldb@gmail.com>
2016-12-02 22:51:45 -0300

    PRESENTERS E TRANSFORMER - Criando Transformer

---
+++ app/Transformers/ProjectTransformer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brito
+ * Date: 02/12/2016
+ * Time: 22:43
+ */
+
+namespace CodeProject\Transformers;
+
+use CodeProject\Entities\Project;
+use League\Fractal\TransformerAbstract;
+
+class ProjectTransformer extends TransformerAbstract
+{
+
+    public function transform(Project $project)
+    {
+        return [
+            'project_id' => $project->id,
+            'project' => $project->name,
+            'description' => $project->description,
+            'progress' => $project->progress,
+            'status' => $project->status,
+            'due_date' => $project->due_date,
+        ];
+    }
+}
