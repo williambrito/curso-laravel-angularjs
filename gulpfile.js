5fcec2323f80d339c9c20a538b25f1c93ef526e6
William Brito <williamrldb@gmail.com>
2016-12-03 19:59:39 -0300

    ANGULAR -> CONFIGURACAO DE AMBIENTE -> Declarando dependências no gulpfile - Parte 2

--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,16 @@ config.bower_path = config.assets_path + '/../bower_components';
 config.build_path_js = config.build_path + '/js';
 config.build_vendor_path_js = config.build_path_js + '/vendor';
 config.vendor_path_js = [
-    config.bower_path + '/jquery/dist/jquery.min.js',
-    config.bower_path + '/bootstrap/dist/js/bootstrap.min.js'
+    config.bower_path + '/angular/angular.min.js',
+    config.bower_path + '/angular-animate/angular-animate.min.js',
+    config.bower_path + '/angular-bootstrap/angular-bootstrap.min.js',
+    config.bower_path + '/angular-messages/angular-messages.min.js',
+    config.bower_path + '/angular-resource/angular-resource.min.js',
+    config.bower_path + '/angular-route/angular-route.min.js',
+    config.bower_path + '/angular-strap/dist/modules/navbar.min.js',
+    config.bower_path + '/angular-strap/dist/modules/navbar.min.js',
+    config.bower_path + '/bootstrap/dist/js/bootstrap.min.js',
+    config.bower_path + '/jquery/dist/jquery.min.js'
 ];
 
 config.build_path_css = config.build_path + '/css';
