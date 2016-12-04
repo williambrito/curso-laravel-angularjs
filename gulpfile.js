f4dcce3d00c3f53a7a08d31f52d3c2b8a86a8dc5
William Brito <williamrldb@gmail.com>
2016-12-03 21:25:11 -0300

    ANGULAR -> CONFIGURACAO DE AMBIENTE -> Tarefa gulp default

--- gulpfile.js
+++ gulpfile.js
@@ -13,16 +13,15 @@ config.bower_path = config.assets_path + '/../bower_components';
 config.build_path_js = config.build_path + '/js';
 config.build_vendor_path_js = config.build_path_js + '/vendor';
 config.vendor_path_js = [
-    config.bower_path + '/angular/angular.min.js',
-    config.bower_path + '/angular-animate/angular-animate.min.js',
-    config.bower_path + '/angular-bootstrap/angular-bootstrap.min.js',
-    config.bower_path + '/angular-messages/angular-messages.min.js',
-    config.bower_path + '/angular-resource/angular-resource.min.js',
-    config.bower_path + '/angular-route/angular-route.min.js',
-    config.bower_path + '/angular-strap/dist/modules/navbar.min.js',
-    config.bower_path + '/angular-strap/dist/modules/navbar.min.js',
+    config.bower_path + '/jquery/dist/jquery.min.js',
     config.bower_path + '/bootstrap/dist/js/bootstrap.min.js',
-    config.bower_path + '/jquery/dist/jquery.min.js'
+    config.bower_path + '/angular/angular.min.js',
+    config.bower_path + '/angular-route/angular-route.min.js',
+    config.bower_path + '/angular-resource/angular-resource.min.js',
+    config.bower_path + '/angular-animate/angular-animate.min.js',
+    config.bower_path + '/angular-messages/angular-messages.min.js',
+    config.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
+    config.bower_path + '/angular-strap/dist/modules/navbar.min.js'
 ];
 
 config.build_path_css = config.build_path + '/css';
@@ -60,23 +59,20 @@ gulp.task('clear-build-folder', function () {
     clean.sync(config.build_path);
 });
 
-gulp.task('watch-dev',['clear-build-folder'], function () {
+gulp.task('default', ['clear-build-folder'], function () {
+    elixir(function (mix) {
+        mix.styles(config.vendor_path_css.concat([config.assets_path + '/css/**/*.css']),
+        'public/css/all.css',config.assets_path);
+
+        mix.scripts(config.vendor_path_js.concat([config.assets_path + '/js/**/*.js']),
+            'public/js/all.js',config.assets_path);
+
+        mix.version(['css/all.css','js/all.js']);
+    });
+});
+
+gulp.task('watch-dev', ['clear-build-folder'], function () {
     liveReload.listen();
     gulp.start('copy-styles','copy-scripts');
     gulp.watch(config.assets_path + '/**', ['copy-styles','copy-scripts']);
-});
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function(mix) {
-    mix.sass('app.scss');
 });
