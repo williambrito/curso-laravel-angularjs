c4f9c65574dce542629b59b51e274b57fea639eb
William Brito <williamrldb@gmail.com>
2016-12-14 15:04:38 -0300

    ANGULAR -> PROJECT CRUD -> Criando lista de seleção de clients

--- resources/assets/js/controllers/project/create.js
+++ resources/assets/js/controllers/project/create.js
@@ -2,11 +2,15 @@ angular.module('app.controllers')
     .controller('projectCreateController', [
         '$scope',
         'projectService',
+        'clientService',
         '$location',
         function ($scope,
                   projectService,
+                  clientService,
                   $location) {
+
             $scope.project = new projectService();
+            $scope.clients = clientService.query();
 
             $scope.save = function () {
                 if ($scope.form.$valid) {
