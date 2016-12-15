3c6df88b8bef660592217a05bba7e0f8a9a9299d
William Brito <williamrldb@gmail.com>
2016-12-15 15:35:55 -0300

    ANGULAR -> TYPEAHEAD E DATEPICKER -> Primeiro contato com typeahead

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,6 +2,7 @@ var app = angular.module('app', [
     'ngRoute',
     'angular-oauth2',
     'ngMessages',
+    'ui.bootstrap.tpls',
     'ui.bootstrap.typeahead',
     'app.controllers',
     'app.filters',
