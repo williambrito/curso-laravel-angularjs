47dafaaeddc4b710dafb71a6f8f930fd7d778b72
William Brito <williamrldb@gmail.com>
2016-12-16 18:05:24 -0300

    ANGULAR -> PROJECT FILE CRUD -> Implementando logica de upload no projectFileNew

--- resources/assets/js/controllers/project-file/create.js
+++ resources/assets/js/controllers/project-file/create.js
@@ -1,22 +1,29 @@
 angular.module('app.controllers')
     .controller('projectFileCreateController', [
         '$scope',
-        //'projectFileService',
         '$location',
         '$routeParams',
+        'Upload',
         function ($scope,
-                  //projectFileService,
                   $location,
-                  $routeParams) {
-            $scope.file = {};//new projectFileService();
-            $scope.file.project_id = $routeParams.id;
+                  $routeParams,
+                  Upload) {
+
+            $scope.file = {
+                project_id: $routeParams.id
+            };
 
             $scope.save = function () {
                 if ($scope.form.$valid) {
-                    $scope.file.$save({id: $routeParams.id}).then(function () {
+                    Upload.upload({
+                        url: 'http://localhost:8000/project/' + $scope.file.project_id + '/file',
+                        data: {
+                            file: $scope.file.file,
+                            name: $scope.file.name,
+                            description: $scope.file.description
+                        }
+                    }).then(function () {
                         $location.path('/project/' + $routeParams.id + '/files');
-                    }, function () {
-
                     });
                 }
             };
