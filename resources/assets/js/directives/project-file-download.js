868dc2730e82e5be614d36519e749a547d7c38d8
William Brito <williamrldb@gmail.com>
2016-12-19 10:29:11 -0300

    ANGULAR -> PROJECT FILE CRUD -> Criando download automatico

--- resources/assets/js/directives/project-file-download.js
+++ resources/assets/js/directives/project-file-download.js
@@ -11,20 +11,33 @@ angular.module('app.directives')
                 link: function (scope, element, attr) {
 
                 },
-                controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
-                    $scope.downloadFile = function () {
-                        var anchor = $element.children()[0];
-                        $(anchor).addClass('disabled');
-                        $(anchor).text('Loading...');
-                        projectFileService.download({id: $attrs.idProject, idFile: $attrs.idFile}, function (data) {
-                            $(anchor).removeClass('disabled');
-                            $(anchor).text('Save File');
-                            $(anchor).attr({
-                                href: 'data:application-octet-stream;base64,' + data.file,
-                                download: data.name
+                controller: ['$scope',
+                    '$element',
+                    '$attrs',
+                    '$timeout',
+                    function ($scope,
+                              $element,
+                              $attrs,
+                              $timeout) {
+                        $scope.downloadFile = function () {
+                            var anchor = $element.children()[0];
+                            $(anchor).addClass('disabled');
+                            $(anchor).text('Loading...');
+                            projectFileService.download({id: $attrs.idProject, idFile: $attrs.idFile}, function (data) {
+                                $(anchor).removeClass('disabled');
+                                $(anchor).text('Save File');
+                                $(anchor).attr({
+                                    href: 'data:application-octet-stream;base64,' + data.file,
+                                    download: data.name
+                                });
+                                $timeout(function () {
+                                    $scope.downloadFile = function () {
+
+                                    };
+                                    $(anchor)[0].click();
+                                });
                             });
-                        });
-                    };
-                }]
+                        };
+                    }]
             };
         }]);
