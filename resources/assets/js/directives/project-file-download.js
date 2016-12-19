ea535fd71c07ad3a722c9ffe04b768732359f703
William Brito <williamrldb@gmail.com>
2016-12-19 10:47:34 -0300

    ANGULAR -> PROJECT FILE CRUD -> Trabalhando com eventos

--- resources/assets/js/directives/project-file-download.js
+++ resources/assets/js/directives/project-file-download.js
@@ -2,40 +2,43 @@ angular.module('app.directives')
     .directive('projectFileDownload', [
         'appConfig',
         'projectFileService',
+        '$timeout',
         function (appConfig,
-                  projectFileService) {
+                  projectFileService,
+                  $timeout) {
 
             return {
                 restrict: 'E',
                 templateUrl: appConfig.baseUrl + '/build/views/templates/project-file-download.html',
                 link: function (scope, element, attr) {
+                    var anchor = element.children()[0];
+                    scope.$on('salvar-arquivo', function (event, data) {
+                        $(anchor).removeClass('disabled');
+                        $(anchor).text('Save File');
+                        $(anchor).attr({
+                            href: 'data:application-octet-stream;base64,' + data.file,
+                            download: data.name
+                        });
+                        $timeout(function () {
+                            scope.downloadFile = function () {
 
+                            };
+                            $(anchor)[0].click();
+                        });
+                    });
                 },
                 controller: ['$scope',
                     '$element',
                     '$attrs',
-                    '$timeout',
                     function ($scope,
                               $element,
-                              $attrs,
-                              $timeout) {
+                              $attrs) {
                         $scope.downloadFile = function () {
                             var anchor = $element.children()[0];
                             $(anchor).addClass('disabled');
                             $(anchor).text('Loading...');
                             projectFileService.download({id: $attrs.idProject, idFile: $attrs.idFile}, function (data) {
-                                $(anchor).removeClass('disabled');
-                                $(anchor).text('Save File');
-                                $(anchor).attr({
-                                    href: 'data:application-octet-stream;base64,' + data.file,
-                                    download: data.name
-                                });
-                                $timeout(function () {
-                                    $scope.downloadFile = function () {
-
-                                    };
-                                    $(anchor)[0].click();
-                                });
+                                $scope.$emit('salvar-arquivo', data);
                             });
                         };
                     }]
