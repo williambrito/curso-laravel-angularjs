d7ec7a33d086d7fe8705b34fff95bce01512a9e0
William Brito <williamrldb@gmail.com>
2016-12-21 11:28:16 -0300

    ANGULAR -> CRUD DE PROJECT TASKS E MEMBER -> Cadastro simplificado de tasks

--- resources/assets/js/controllers/project-task/index.js
+++ resources/assets/js/controllers/project-task/index.js
@@ -3,9 +3,23 @@ angular.module('app.controllers')
         '$scope',
         'projectTaskService',
         '$routeParams',
+        'appConfig',
         function ($scope,
                   projectTaskService,
-                  $routeParams) {
+                  $routeParams,
+                  appConfig) {
+
+            $scope.task = new projectTaskService();
+
+            $scope.save = function () {
+                if ($scope.form.$valid) {
+                    $scope.task.status = appConfig.projectTask.status[0].value;
+                    $scope.task.$save({id: $routeParams.id}).then(function (data) {
+                        $scope.task = new projectTaskService();
+                        $scope.tasks.unshift(data);
+                    });
+                }
+            };
 
             $scope.loadTask = function () {
                 $scope.tasks = projectTaskService.query({
