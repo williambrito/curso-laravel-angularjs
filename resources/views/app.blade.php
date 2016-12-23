62f014906d3b0ac846a5f110264a49fff09829e4
William Brito <williamrldb@gmail.com>
2016-12-23 15:41:08 -0300

    ANGULAR -> AUTORIZACAO -> Instalando angular-http-auth e modals

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -81,6 +81,7 @@
 		<script src="{{ asset('build/js/vendor/query-string.js') }}"></script>
 		<script src="{{ asset('build/js/vendor/angular-oauth2.min.js') }}"></script>
 		<script src="{{ asset('build/js/vendor/ng-file-upload.min.js') }}"></script>
+		<script src="{{ asset('build/js/vendor/http-auth-interceptor.min.js') }}"></script>
 		<!-- app -->
 		<script src="{{ asset('build/js/app.js') }}"></script>
 		<script src="{{ asset('build/js/controllers/login.js') }}"></script>
