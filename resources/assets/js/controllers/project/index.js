6000497ef4cc88ef873e4cb908e013072a470743
William Brito <williamrldb@gmail.com>
2017-01-12 09:24:56 -0300

    ANGULAR -> AJUSTES E FUNCIONALIDADES -> Configurando a paginação

--- resources/assets/js/controllers/project/index.js
+++ resources/assets/js/controllers/project/index.js
@@ -1,5 +1,28 @@
 angular.module('app.controllers')
-    .controller('projectIndexController', ['$scope', 'projectService',
-        function ($scope, projectService) {
-            $scope.projects = projectService.query();
+    .controller('projectIndexController', [
+        '$scope',
+        'projectService',
+        function ($scope,
+                  projectService) {
+
+            $scope.projects = [];
+            $scope.totalProjects = 0;
+            $scope.projectsPerPage = 15;
+
+            $scope.pagination = {
+                current: 1
+            };
+
+            $scope.pageChanged = function (newPage) {
+                getResultsPage(newPage);
+            };
+
+            function getResultsPage(pageNumber) {
+                projectService.query({}, function (data) {
+                    $scope.projects = data;
+                    //$scope.totalProjects = result.data.Count
+                });
+            }
+
+            getResultsPage(1);
         }]);
